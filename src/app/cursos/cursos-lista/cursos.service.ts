7ed6ff8c44f20b57a52e7c84c569af00e53c2fae
Andre Viana <code.andre3@gmail.com>
2021-07-12 10:34:59 -0300

    put curso.ts and move to servise.ts <Curso[]>

--- src/app/cursos/cursos-lista/cursos.service.ts
+++ src/app/cursos/cursos-lista/cursos.service.ts
@@ -1,9 +1,17 @@
+import { Curso } from './curso';
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class CursosService {
+  
+  private readonly API = '  http://localhost:3000/cursos'
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  listar() {
+    return this.http.get<Curso[]>(this.API);
+  }
 }
